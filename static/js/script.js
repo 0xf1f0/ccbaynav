ce9944eb3f3084dc7f5d8e90a4060f6e4f46da94
0xf1f0 <oobey1@aol.com>
2017-11-09 00:14:38 -0500

    Center map on corpus christi bay

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,9 @@
  * Initialize and display Google maps
  */
 
-var initial_lat = 27.802912;            //  The corresponding latitude of map center at initialization.
-var initial_long = -97.355775;          //  The corresponding longitude of map center at initialization.
-var initial_zoom = 12;                  //  The corresponding zoom of map center at initialization.
+var initial_lat = 27.75875;            //  The corresponding latitude of map center at initialization.
+var initial_long = -97.245233;          //  The corresponding longitude of map center at initialization.
+var initial_zoom = 11;                  //  The corresponding zoom of map center at initialization.
 var map;
 
 function initMap() {
