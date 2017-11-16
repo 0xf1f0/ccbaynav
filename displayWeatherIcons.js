69e14585aeb78bb777439c08d8a5164d708d32fe
Dylan Jones <dylanjones199413@gmail.com>
2017-11-16 11:53:22 -0600

    Javascript for the displaying of weather icons updated. Possibly completed. (?)

--- displayWeatherIcons.js
+++ displayWeatherIcons.js
@@ -1,54 +1,91 @@
 /**
  * Import the weather icons from the json file and display the correct icon for current weather condition
- *
- * https://stackoverflow.com/questions/4828207/how-to-use-a-json-file-in-javascript
- * https://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript
+ * https://www.youtube.com/watch?v=gwTkd52Dm48 "How to Get JSON data to HTML page" by: Help People
+ * https://www.youtube.com/watch?v=6mT3r8Qn1VY "Loading JSON data from a URL (Asynchronous Callbacks!)" by: The Coding Train
+ * https://www.youtube.com/watch?v=ecT42O6I_WI "Working with APIs in Javascript" by: The Coding Train
  */
-//POSSIBLE OPTION 1
-$.getJSON("icon.json", function(data) {
-    console.log(data);
-    // data is a JavaScript object now. Handle it as such
 
-});
-
-/*
- * https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
-
-
- The correct method - create a new XMLHttpRequest
-    The clue here is the jQuery method $.getJSON() which is shorthand for $.ajax(). It may seem an odd approach requesting a
-    local file in this way but it offers the most flexibility with minimum fuss.
-*/
-
-//POSSIBLE OPTION 2
- function loadJSON(callback)
+ /*
+    - Preload the json file containing the icons
+    - Read in weather json for the area we want via URL
+    - Grab the icon tag associated with the current weather in the area from the
+      weather json we got via URL
+    - If the icon exists...
+        - Use switch case to call the correct iconImg from the icon.json file
+        - Once called the icon will be displayed onscreen
+ */
+ function preload()
  {
-
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'icon.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function ()
-                            {
-                                  if (xobj.readyState == 4 && xobj.status == "200")
-                                  {
-                                    // Required use of an anonymous callback as .open will NOT return a value but simply
-                                    // returns undefined in asynchronous mode
-                                    callback(xobj.responseText);
-                                  }
-                            };
-    xobj.send(null);
+    iconImg = loadJSON("icon.json");
  }
-/*
-The function above will create a new instance of a XMLHttpRequest and load asynchronously the contents of my_data.json.
-I have gone with asynchronous but you can change the argument to false if you want a synchronous load. Thankfully all
-modern browsers support the native JSON.parse method. Remember our anonymous callback? here's how you use it.
-#Usage
-*/
-function init()
-{
- loadJSON(function(response)
+ function setup()
  {
-  // Parse JSON string into object
-    var actual_JSON = JSON.parse(response);
- });
-}
+    loadJSON("URL FOR OUR JSON WEATHER INFO GOES HERE",gotData);
+    //if error occurs try...
+    //loadJSON(icon.json,gotData,'jsonp');
+ }
+
+ function gotData(data)
+ {
+    var icon = data.icon;
+    if(icon)
+    {
+        switch(icon)
+        {
+            case 01d:
+                output.innerHTML = iconImg.01d;
+                break;
+            case 01n:
+                output.innerHTML = iconImg.01n;
+                break;
+            case 02d:
+                output.innerHTML = iconImg.01d;
+                break;
+            case 02n:
+                output.innerHTML = iconImg.01n;
+                break;
+            case 03d:
+                output.innerHTML = iconImg.01d;
+                break;
+            case 03n:
+                output.innerHTML = iconImg.01n;
+                break;
+            case 04d:
+                output.innerHTML = iconImg.01d;
+                break;
+            case 04n:
+                output.innerHTML = iconImg.01n;
+                break;
+            case 09d:
+                output.innerHTML = iconImg.01d;
+                break;
+            case 09n:
+                output.innerHTML = iconImg.01n;
+                break;
+            case 10d:
+                output.innerHTML = iconImg.01d;
+                break;
+            case 10n:
+                output.innerHTML = iconImg.01n;
+                break;
+            case 11d:
+                output.innerHTML = iconImg.01d;
+                break;
+            case 11n:
+                output.innerHTML = iconImg.01n;
+                break;
+            case 13d:
+                output.innerHTML = iconImg.01d;
+                break;
+            case 13n:
+                output.innerHTML = iconImg.01n;
+                break;
+            case 50d:
+                output.innerHTML = iconImg.01d;
+                break;
+            case 50n:
+                output.innerHTML = iconImg.01n;
+                break;
+        }
+    }
+ }
