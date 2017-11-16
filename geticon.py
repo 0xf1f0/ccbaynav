8608b701d350a5a7b27abd6f8565947ffdf54c3f
Dylan Jones <dylanjones199413@gmail.com>
2017-11-15 20:29:07 -0600

    JSON list of icons and geticon module.

--- geticon.py
+++ geticon.py
@@ -1,26 +1,23 @@
-# TODO: SECURITY WARNING: keep the secret key used in production secret!
-# TODO: Regenerate secret key and place in config.json
-# TODO: See project checklist link above before making API_KEYS public
-# TODO: Change config.json path to Linux format
+# TODO: Change icon.json path to Linux format
 
 import os.path
 import json
 
 """
-    This function takes in an api_name and returns a corresponding api_key from the config.json file.
-    Returns "None" if the api_name is not in the dictionary or config.json file does not exist
+    This function takes in an icon_name and returns a corresponding icon_key from the icon.json file.
+    Returns "None" if the icon_name is not in the dictionary or icon.json file does not exist
     Example:    {
                     "01d": "http://openweathermap.org/img/w/01d.png",
                 }
 """
 
 
-def search_key(api_name):
+def search_key(icon_name):
     # TODO: Change '.\icon.json' to './icon.sjon' when in Linux environment
-    key_path = os.path.basename('.\icon.json')
+    icon_path = os.path.basename('.\icon.json')
 
     #   Check if the file exist
-    if os.path.exists(key_path):
+    if os.path.exists(icon_path):
         """
             Read the JSON file into a dictionary variable [dict_json]
             Search and return the key that matches the API name
@@ -28,12 +25,12 @@ def search_key(api_name):
         """
 
         try:
-            f = open(key_path, 'r')
+            f = open(icon_path, 'r')
         except IOError as e:
             raise type(e)(e.message)
         else:
             dict_json = json.load(f)
-            return dict_json.get(api_name)
+            return dict_json.get(icon_name)
     else:
-        print("File: %s" % key_path + " not found")
+        print("File: %s" % icon_path + " not found")
         return None
