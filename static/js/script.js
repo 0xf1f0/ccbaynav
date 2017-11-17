0e567a32e9e1404bf9735f4b41f67eae2a54b907
0xf1f0 <oobey1@aol.com>
2017-11-17 02:34:56 -0500

    Add multiple markers with custom icon

--- static/js/script.js
+++ static/js/script.js
@@ -4,9 +4,11 @@
 
 var initial_lat = 27.75875;            //  The corresponding latitude of map center at initialization.
 var initial_lng = -97.245233;          //  The corresponding longitude of map center at initialization.
-var initial_zoom = 11;                  //  The corresponding zoom of map center at initialization.
+var initial_zoom = 11;                 //  The corresponding zoom of map center at initialization.
 var map;
 
+
+//This function initializes the Google Maps
 
 function initMap() {
     var latlng = {lat: initial_lat, lng: initial_lng};
@@ -16,10 +18,47 @@ function initMap() {
         mapTypeId: 'roadmap'
     });
 
-    var marker = new google.maps.Marker({
-        position: latlng,
-        map: map,
-        title: 'Corpus Christi Bay'
+    //Data Stations
+    var markers = [['Aransas Pass', 27.8366, -97.0391],
+        ['Port_Aransas', 27.8397, -97.0725],
+        ['USS Lexington', 27.8149, -97.3892],
+        ['Bob Hall Pier', 27.5800, -97.2167]
+    ];
+
+    // Info Window Content
+    var infoWindowContent = [
+        ['<div class="info_content">' + '<h3>Station: Aransas Pass</h3>' + '</div>'],
+        ['<div class="info_content">' + '<h3>Station: Port_Aransas</h3>' + '</div>'],
+        ['<div class="info_content">' + '<h3>Station: USS Lexington</h3>' + '</div>'],
+        ['<div class="info_content">' + '<h3>Station: Bob Hall Pier</h3>' + '</div>'],];
+
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+
+    /* Loop through the array of markers(data stations)
+       Place the markers on the map
+     */
+    for (i = 0; i < markers.length; i++) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            icon: 'static/media/mapicons/lighthouse.png',
+            title: markers[i][0]
+        });
+
+        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            return function () {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+    }
+
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function (event) {
+        this.setZoom(initial_zoom);
+        google.maps.event.removeListener(boundsListener);
     });
 }
 
@@ -36,7 +75,6 @@ function initMap() {
         date = moment(new Date());
         date_time.innerHTML = date.tz(timezone).format(format);
     }
-
     setInterval(function () {
         updateClock(timeElement);
     }, 1000);
