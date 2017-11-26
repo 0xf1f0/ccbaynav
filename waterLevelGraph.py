10b0c22ab2473b3683d2296b3bc91ca0ddef61a2
Dylan Jones <dylanjones199413@gmail.com>
2017-11-26 13:42:00 -0600

    adding waterLevelGraph.py updating

--- waterLevelGraph.py
+++ waterLevelGraph.py
@@ -1,17 +1,21 @@
-from bokeh.plotting import figure, output_file, show
+from bokeh.io import output_file, save
+from bokeh.models import GeoJSONDataSource
+from bokeh.plotting import figure
+
+geo_source = GeoJSONDataSource(geojson="LOCATION OF THE JSON DATA FILE")
 
 # prepare some data
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
-
-# output to static HTML file
-output_file("lines.html")
+# x = [1, 2, 3, 4, 5]
+# y = [6, 7, 2, 4, 5]
 
 # create a new plot with a title and axis labels
 p = figure(plot_width=729, plot_height=485, title="Water Level Graph", x_axis_label='Time', y_axis_label='Height (ft.)')
 
 # add a line renderer with legend and line thickness
-p.line(x, y, legend="Water Level", line_width=2)
+p.line(x='x', y='y', legend="Water Level", line_width=2, source=geo_source)
 
-# show the results
-show(p)
+# output to static HTML file
+output_file("waterLvlGraph.html")
+
+# save the results
+save(p, filename="waterLvlGraph.html", title="Water Level Graph")
