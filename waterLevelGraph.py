b2d289077e3fdbb354d505729da179f7d54d0ab5
Dylan Jones <dylanjones199413@gmail.com>
2017-11-26 16:14:03 -0600

    adding a python file for graph testing

--- waterLevelGraph.py
+++ waterLevelGraph.py
@@ -1,21 +1,34 @@
-from bokeh.io import output_file, save
-from bokeh.models import GeoJSONDataSource
+# import NOAA_request
+import json
+
+from bokeh.io import output_file, show
 from bokeh.plotting import figure
 
-geo_source = GeoJSONDataSource(geojson="LOCATION OF THE JSON DATA FILE")
+# noaaData = NOAA_request.get_noaa_data()
+# print(noaaData)
+waterLvl_File = open("Water_Levels.json", "r")
+jWL = json.load(waterLvl_File)
+waterLvl_File.close()
+pWL = json.dumps(jWL)
 
 # prepare some data
-# x = [1, 2, 3, 4, 5]
-# y = [6, 7, 2, 4, 5]
+y = []
+x = []
+a = 0
+for z in pWL:
+    y.append(pWL["data"][z].get("v"))
+    x.append(pWL["data"][z].get("t").split(" ")[1:][0])
+    a+=1
 
 # create a new plot with a title and axis labels
 p = figure(plot_width=729, plot_height=485, title="Water Level Graph", x_axis_label='Time', y_axis_label='Height (ft.)')
 
 # add a line renderer with legend and line thickness
-p.line(x='x', y='y', legend="Water Level", line_width=2, source=geo_source)
+p.line(x='x', y='y', legend="Water Level", line_width=2)
 
 # output to static HTML file
 output_file("waterLvlGraph.html")
 
 # save the results
-save(p, filename="waterLvlGraph.html", title="Water Level Graph")
+show(p)
+#save(p, filename="waterLvlGraph.html", title="Water Level Graph")
