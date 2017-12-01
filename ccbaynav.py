f10800c73632001d1f157032887da1ccb7c3d62a
0xf1f0 <oobey1@aol.com>
2017-12-01 16:15:06 -0500

    Gabby (#3)
    * Javascript for displaying weather icons updated.
    
    * Python module to get the corresponding weather icon based on the received json data
    
    Replacing the Javascript version of this code.
    
    * minor tweaks
    
    * Added get_weather_img_key function.
    
    There are two versions of the function:
    - One should be used if the API weather data is being obtained by file
    - The other should be used if the API weather data is being obtained by URL
    
    Some data needs to be added to parts of the functions.
    I tried to have those areas stand out.
    
    * Added a TODO to geticon.py
    
    * adding waterLevelGraph.py
    
    * adding waterLevelGraph.py
    
    * adding waterLevelGraph.py updating
    
    * adding waterLevelGraph.py updating
    
    * adding waterLevelGraph.py updating
    
    * adding waterLevelGraph.py updating
    
    * adding waterLevelGraph.py updating
    
    * adding a python file for graph testing
    
    * adding a python file for graph testing
    
    * adding a python file for graph testing
    
    * water level graph works. except for populating a list of points from the json data. having issues with the for loop on lines 18-30.
    
    * water level graph works. except for populating a list of points from the json data. having issues with the for loop on lines 18-30.
    
    * water level graph works. except for populating a list of points from the json data. having issues with the for loop on lines 18-30.
    
    * water level graph works. except for populating a list of points from the json data. having issues with the for loop on lines 18-30.
    
    * Made the graph read in the correct json, but the time is out of order
    
    * Delete config.json
    
    * Delete config.json
    
    * Python module to get the corresponding weather icon based on the received json data
    
    Replacing the Javascript version of this code.
    
    * minor tweaks
    
    * Added get_weather_img_key function.
    
    There are two versions of the function:
    - One should be used if the API weather data is being obtained by file
    - The other should be used if the API weather data is being obtained by URL
    
    Some data needs to be added to parts of the functions.
    I tried to have those areas stand out.
    
    * adding waterLevelGraph.py
    
    * adding waterLevelGraph.py
    
    * adding waterLevelGraph.py updating
    
    * adding waterLevelGraph.py updating
    
    * adding waterLevelGraph.py updating
    
    * adding waterLevelGraph.py updating
    
    * adding waterLevelGraph.py updating
    
    * adding a python file for graph testing
    
    * adding a python file for graph testing
    
    * adding a python file for graph testing
    
    * water level graph works. except for populating a list of points from the json data. having issues with the for loop on lines 18-30.
    
    * water level graph works. except for populating a list of points from the json data. having issues with the for loop on lines 18-30.
    
    * water level graph works. except for populating a list of points from the json data. having issues with the for loop on lines 18-30.
    
    * water level graph works. except for populating a list of points from the json data. having issues with the for loop on lines 18-30.
    
    * Made the graph read in the correct json, but the time is out of order
    
    * Graph is printing with time in order and with time label, using jon input
    
    * Fixed api save path and deleted local jsons
    
    * Minor filename changes
    
    * Rearranged jsons generated with NOAA data to be separated by variables rather than stations
    
    * Made all the graphs generate at once with all locations printed on graphs (need colors and units still)
    
    * Removed python list junk in graph display

--- ccbaynav.py
+++ ccbaynav.py
@@ -1,6 +1,5 @@
 # TODO: Change debug mode to false during production
 from flask import Flask, render_template
-
 from bokehGraphs import graph_generator
 
 app = Flask(__name__)
@@ -11,8 +10,9 @@ def index():
     #   Render index.html as the default page
     #   See index.html in ../templates/
     script, div_list = graph_generator()
-    return render_template("index.html", script=script, div=div_list)
-
+    # need to add more divs here if you want to add more graphs
+    return render_template("index.html", script=script, div=div_list[0], div1=div_list[1], div2=div_list[2],
+                           div3=div_list[3], div4=div_list[4])
 
 if __name__ == '__main__':
     app.run(debug=False)
